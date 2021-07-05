886d0d5992e56f1f080941c347e5056d89ed7cbb
sebastaines <86328004+sebastaines@users.noreply.github.com>
2021-07-06 11:37:50 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("y")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("n")
+})
+input.onGesture(Gesture.Shake, function () {
+    dice = randint(1, 6)
+    basic.showNumber(dice)
+    radio.sendNumber(dice)
+})
+let dice = 0
+radio.setGroup(13)
